a0d83c3709da9e1432e6b10c07cf6e3004b394f2
PrachiPhatak <phatak.prachi@gmail.com>
2021-06-11 11:27:41 -0500

    git testing

--- js/api/keys.js
+++
@@ -1,2 +0,0 @@
-const MAPBOX_TOKEN ='pk.eyJ1IjoicHJhY2hpcGhhdGFrIiwiYSI6ImNrcHBuODdhZTBid2gydXBheGhyd3p3eGEifQ.lFAZWrlALu4kbSdTG-Qq8A';
-const OPEN_WEATHER_TOKEN ='11521718a0eeb7995e425fbfe2b254cd';
